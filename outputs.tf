c052a2fe9dbc646ed8dc9c2f358fa8ffa59b040c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:18:22 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_hub_security_partner_providers" {
+  description = "All virtual_hub_security_partner_provider resources"
+  value       = azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers
+}
+
