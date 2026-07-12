26fd5ef9a0aed19bc52d024860d7204757ea90ff
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "virtual_hub_security_partner_providers" {
-  description = "All virtual_hub_security_partner_provider resources"
-  value       = azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers
-}
 output "virtual_hub_security_partner_providers_location" {
-  description = "List of location values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.location]
+  description = "Map of location values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.location }
 }
 output "virtual_hub_security_partner_providers_name" {
-  description = "List of name values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.name]
+  description = "Map of name values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.name }
 }
 output "virtual_hub_security_partner_providers_resource_group_name" {
-  description = "List of resource_group_name values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.resource_group_name]
+  description = "Map of resource_group_name values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.resource_group_name }
 }
 output "virtual_hub_security_partner_providers_security_provider_name" {
-  description = "List of security_provider_name values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.security_provider_name]
+  description = "Map of security_provider_name values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.security_provider_name }
 }
 output "virtual_hub_security_partner_providers_tags" {
-  description = "List of tags values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.tags]
+  description = "Map of tags values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.tags }
 }
 output "virtual_hub_security_partner_providers_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_security_partner_providers"
-  value       = [for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_security_partner_providers, keyed the same as var.virtual_hub_security_partner_providers"
+  value       = { for k, v in azurerm_virtual_hub_security_partner_provider.virtual_hub_security_partner_providers : k => v.virtual_hub_id }
 }
 
